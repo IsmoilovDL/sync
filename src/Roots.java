b417a35d9705e2d31bc881c220991b665bf41937
work <ismoilov92@gmail.com>
2022-02-14 18:00:38 +0300

    замена на ArrayList

--- src/Roots.java
+++ src/Roots.java
@@ -1,17 +1,18 @@
 import java.io.File;
+import java.util.ArrayList;
 
 public class Roots {
     private  final String keyfile="synceKey.txt";
 
-    public String[] activDisks(){
-        String[] disks = new String[5];
+    public ArrayList<String> activDisks(){
+        ArrayList<String> disklist=new ArrayList<>();
         File[] roots = File.listRoots();
         for (int i=0; i<roots.length; i++){
             File keysynce=new File(roots[i].toString()+keyfile);
             if(keysynce.exists()) {
-                disks[i] =  roots[i].toString() + keyfile;
+                disklist.add(roots[i].toString() + keyfile);
             }
         }
-        return disks;
+        return disklist;
     }
 }
--- src/Synce.java
+++ src/Synce.java
@@ -1,10 +1,9 @@
-import java.util.Arrays;
 
 public class Synce {
     public static void main(String[] args) {
 
        Roots disks=new Roots();
 
-       System.out.println(Arrays.toString(disks.activDisks()));
+       System.out.println(disks.activDisks());
     }
 }
