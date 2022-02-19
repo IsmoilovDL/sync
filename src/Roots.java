5eced069625688dce98b228956129f71b8e37ee1
311 <ismoilov92@gmail.com>
2022-02-19 23:44:15 +0300

    рефакторинг Roots

--- src/Roots.java
+++ src/Roots.java
@@ -6,37 +6,35 @@ import java.util.List;
 public class Roots {
     private static final String keyfile="synceKey.txt";
 
-    //Находить диск с файлом для синхронизации
-    public List<String> getActiveDisks(){
-        List<String> disklist=new ArrayList<>();
+    //Список всех дисков
+    public ArrayList<String> getAllDisks(){
+        ArrayList<String> disklist=new ArrayList<>();
         File[] roots = File.listRoots();
         for (int i=0; i<roots.length; i++){
-            File keysynce=new File(roots[i].toString()+keyfile);
+            disklist.add(roots[i].toString());
+        }
+        return disklist;
+    }
+
+    //Находить диск с файлом для синхронизации
+    public ArrayList<String> getActiveDisks(){
+       ArrayList<String> roots=getAllDisks();
+       ArrayList<String> disklist=new ArrayList<>();
+        for (int i=0; i<roots.size(); i++){
+            File keysynce=new File(roots.get(i)+keyfile);
             if(keysynce.exists()) {
-                disklist.add(roots[i].toString());
+                disklist.add(roots.get(i));
             }
         }
         return disklist;
     }
 
-    //Список всех дисков
-    public List<String> getAllDisks(){
-        List<String> disklist=new ArrayList<>();
-        File[] roots = File.listRoots();
-        for (int i=0; i<roots.length; i++){
-                disklist.add("["+i+"]"+roots[i].toString());
-        }
-        return disklist;
-    }
+
 
     //создает файл ключ в выбранном диске
     public void setActiveDisk(int diskNumber) throws IOException {
-        List<String> disklist=new ArrayList<>();
-        File[] roots = File.listRoots();
-        for (int i=0; i<roots.length; i++){
-            disklist.add(roots[i].toString());
-        }
-        File newFile=new File(disklist.get(diskNumber)+keyfile);
+        ArrayList<String> roots=getAllDisks();
+        File newFile=new File(roots.get(diskNumber)+keyfile);
         newFile.createNewFile();
     }
 }
--- src/Synce.java
+++ src/Synce.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 public class Synce {
     public static void main(String[] args) {
 
-        Roots disks=new Roots();
         DirectoryTree directoryTree=new DirectoryTree();
         directoryTree.DirectoryAndFiles("D:\\Zona Downloads");
 
