109b2ad837f3d41586ae4b603a52d60ca56741ee
311 <ismoilov92@gmail.com>
2022-02-20 23:59:57 +0300

    list

--- src/DirectoryTree.java
+++ src/DirectoryTree.java
@@ -4,13 +4,20 @@ public class DirectoryTree {
 
     public void DirectoryAndFiles(String path){
         File dir = new File(path);
-        System.out.println(dir.getName() + (!dir.isDirectory() ? "  -" : ""));
+       // System.out.println(dir.getName() + (!dir.isDirectory() ? "  -" : ""));
+        StringBuilder fileAbsolutPath=new StringBuilder();
         if (dir.isDirectory()) {
+            fileAbsolutPath.append("**"+dir.getName());
+            System.out.println(fileAbsolutPath);
             File[] list = dir.listFiles();
             if (list != null)
                 for (File name : list)
                     DirectoryAndFiles(name.getPath());
 
+        }
+        else {
+            fileAbsolutPath.append(dir.getName());
+            System.out.println(fileAbsolutPath);
         }
     }
 }
